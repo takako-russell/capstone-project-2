c642845fe91f84e04069c537dcc9e905b44b4060
Takako Russell <takako.russell079@gmail.com>
2024-07-22 21:18:26 -0400

    dropped nullable for names in the table

--- backend/models.py
+++ backend/models.py
@@ -10,8 +10,8 @@ class User(db.Model):
     id = db.Column(db.Integer,primary_key=True,unique=True)
     email = db.Column(db.Text,nullable=False,unique=True)
     external_id = db.Column(db.Text,nullable=True)
-    given_name = db.Column(db.Text,nullable=False)
-    family_name = db.Column(db.Text,nullable=False)
+    given_name = db.Column(db.Text)
+    family_name = db.Column(db.Text)
     
     def to_json(self):
         return {
