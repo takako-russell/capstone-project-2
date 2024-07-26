45ecc1a38ed8c5213d85ff319b343ea4975fc3f9
Takako Russell <takako.russell079@gmail.com>
2024-07-25 22:17:26 -0400

    continued render

--- backend/main.py
+++ backend/main.py
@@ -375,6 +375,6 @@ def editCategory(user, category_id):
     
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 10000))
     connect_db(app)
     app.run(host='0.0.0.0', port=port)
