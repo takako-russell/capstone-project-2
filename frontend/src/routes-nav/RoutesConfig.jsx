29c2ac33009b3b42a7c326d4553668ac6489149c
Takako Russell <takako.russell079@gmail.com>
2024-08-16 15:29:51 -0400

    Added ItemList and ExpenseDetails components

--- frontend/src/routes-nav/RoutesConfig.jsx
+++ frontend/src/routes-nav/RoutesConfig.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import RoutesOverride from "./Routes";
 import ItemList from "../item/ItemList";
 import StoreList from "../store/StoreList";
+import ExpenseDetails from "../displayExpDetails/ExpenseDetails";
 
 const RoutesConfig = React.memo(
   ({ stores, searchStores, removeStore, addStore, setExpense }) => {
