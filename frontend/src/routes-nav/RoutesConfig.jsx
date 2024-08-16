1f0e41258470505dfb4ceca6704fb05bb0ed8a41
Takako Russell <takako.russell079@gmail.com>
2024-08-16 15:28:07 -0400

    changed RoutesConfig

--- frontend/src/routes-nav/RoutesConfig.jsx
+++ frontend/src/routes-nav/RoutesConfig.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import RoutesOverride from "./Routes";
+import ItemList from "../item/ItemList";
 import StoreList from "../store/StoreList";
 
 const RoutesConfig = React.memo(
